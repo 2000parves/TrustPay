915f831a216f3bba4f293c120d0604f3ea09de74
Parves Mosarof <parvesmosarof2@gmail.com>
2025-09-04 05:41:44 +0600

    added react router

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
+import { RouterProvider } from "react-router";
+import { router } from "./routes/index.tsx";
 import { HelmetProvider } from "react-helmet-async";
-import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
       <Provider store={store}>
-    <App />
+ <RouterProvider router={router} />
+
       </Provider>
     </HelmetProvider>
   </StrictMode>
