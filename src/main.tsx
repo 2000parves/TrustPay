0210b0c0159aec493d9ba7337cdab8939478a6e8
Parves Mosarof <parvesmosarof2@gmail.com>
2025-09-04 05:43:53 +0600

    added sonner toast

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,14 @@ import { store } from "./redux/store.ts";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/index.tsx";
 import { HelmetProvider } from "react-helmet-async";
+import { Toaster } from "./components/ui/sonner.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
       <Provider store={store}>
  <RouterProvider router={router} />
-
+  <Toaster richColors />
       </Provider>
     </HelmetProvider>
   </StrictMode>
