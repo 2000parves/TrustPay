900fe9cbd4de06fe03d078696462a4efa88b854a
Parves Mosarof <parvesmosarof2@gmail.com>
2025-09-04 05:36:24 +0600

    added redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 import { HelmetProvider } from "react-helmet-async";
-import App from "./App";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
-      <App />
+      <Provider store={store}>
+    <App />
+      </Provider>
     </HelmetProvider>
   </StrictMode>
 );
